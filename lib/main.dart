4ed869742cdef2b32e00bb483ff42d20b0f0bbb0
赵群涛 <zqt1013@163.com>
2019-07-24 17:16:37 +0800

    动画

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,22 @@ import 'package:flutter_app/FocusNodeController.dart';
 import 'package:flutter_app/TabbarController.dart';
 import 'package:flutter_app/Animation_Demo1.dart';
 import 'package:flutter_app/Animation_Demo2.dart';
+import 'package:flutter_app/ClipDemoPage.dart';
+import 'package:flutter_app/ScrollListenerDemoPage.dart';
+import 'package:flutter_app/ScrollToIndexDemoPage.dart';
+import 'package:flutter_app/ScrollToIndexDemoPage2.dart';
+import 'package:flutter_app/GridView1.dart';
+import 'package:flutter_app/TextLineHeightDemoPage.dart';
+import 'package:flutter_app/TransformDemoPage.dart';
+import 'package:flutter_app/RefreshDemoPage.dart';
+import 'package:flutter_app/RefreshDemoPage2.dart';
+import 'package:flutter_app/PositionedDemoPage.dart';
+import 'package:flutter_app/FloatingTouchDemoPage.dart';
+import 'package:flutter_app/SliverListDemoPage.dart';
+
+
+
+
 
 void main() => runApp(MyApp());
 
@@ -82,23 +98,24 @@ const routerName = [
   "滑动监听 例子",
   "滑动到指定位置 例子",
   "滑动到指定位置2 例子",
+  "二维网格列表",
   "Transform 例子",
   "文本行间距 例子",
   "简单上下刷新 例子",
   "简单上下刷新2 例子",
-  "简单上下刷新3 例子",
+  // "简单上下刷新3 例子",
   "绝对定位 例子",
-  "弹出提示框 例子",
-  "Tag 例子",
-  "共享元素 例子",
-  "状态栏颜色 例子",
-  "键盘相关 例子",
-  "动画 例子",
-  "动画2 例子",
+  // "弹出提示框 例子",
+  // "Tag 例子",
+  // "共享元素 例子",
+  // "状态栏颜色 例子",
+  // "键盘相关 例子",
+  // "动画 例子",
+  // "动画2 例子",
   "悬浮触摸控件 例子",
-  "全局字体大小 例子",
-  "富文本 例子",
-  "viewpager 例子",
+  // "全局字体大小 例子",
+  // "富文本 例子",
+  // "viewpager 例子",
   "滑动停靠 例子",
   "验证码输入框 例子",
   "自定义布局 例子",
@@ -124,7 +141,31 @@ Map<String, WidgetBuilder> routers = {
     return new AnimationContainerDemoPage();
   },"widget/Animation2":(context) {
     return new AnitionDemo2();
+  },"widget/clip":(context) {
+    return new ClipDemoPage();
+  },"widget/scroller":(context) {
+    return new ScrollListenerDemoPage();
+  },"widget/scrollerto" :(context) {
+    return new ScrollToIndexDemoPage();
+  },"widget/scrollerto2" :(context) {
+    return new ScrollToIndexDemoPage2();
+  },"widget/GridView1":(context){
+    return new GridView1();
+  },"widget/ClipDemoPage":(context){
+    return new TransformDemoPage();
+  },"widget/TextLineHeightDemoPage":(context){
+    return new TextLineHeightDemoPage();
+  },"widget/RefreshDemoPage":(context){
+    return new RefreshDemoPage();
+  },"widget/RefreshDemoPage2":(context){
+    return new RefreshDemoPage2();
+  },"widget/PositionedDemoPage":(context){
+    return new PositionedDemoPage();
+  },"widget/SliverListDemoPage":(context){
+    return new SliverListDemoPage();
   }
+
+
 
 
 };
